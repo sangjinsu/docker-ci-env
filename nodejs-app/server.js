0f1eb2ba534068404bad71472baa5186816d5449
jinsuSang <climbplant39@kakao.com>
2021-12-27 17:22:06 +0900

    docker nodejs app

---
+++ nodejs-app/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const PORT = 8080
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello!')
+})
+
+app.listen(PORT, () => {
+  console.log('Server start in http://localhost:' + PORT)
+})
